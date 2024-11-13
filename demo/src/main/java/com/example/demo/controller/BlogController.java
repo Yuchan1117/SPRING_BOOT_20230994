3f08bdc7d985b0c5f5f3d500fb3f911618b2c248
admin <dbcksthd123@naver.com>
2024-11-13 10:45:28 +0900

    8주차

--- demo/src/main/java/com/example/demo/controller/BlogController.java
+++ demo/src/main/java/com/example/demo/controller/BlogController.java
@@ -4,13 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.model.domain.TestDB;
 import com.example.demo.model.repository.TestRepository;
@@ -61,11 +65,21 @@ public class BlogController {
     }
 
     @GetMapping("/board_list") // 새로운 게시판 링크 지정
-    public String board_list(Model model) {
-        List<Board> list = blogService.findAll(); // 게시판 전체 리스트
+    public String board_list(Model model, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "") String keyword) {
+        PageRequest pageable = PageRequest.of(page, 3); // 한 페이지의 게시글 수
+        Page<Board> list; // Page를 반환
+
+        if (keyword.isEmpty()) {
+            list = blogService.findAll(pageable); // 기본 전체 출력(키워드 x)
+        } else {
+            list = blogService.searchByKeyword(keyword, pageable); // 키워드로 검색
+        }
         model.addAttribute("boards", list); // 모델에 추가
+        model.addAttribute("totalPages", list.getTotalPages()); // 페이지 크기
+        model.addAttribute("currentPage", page); // 페이지 번호
+        model.addAttribute("keyword", keyword); // 키워드
         return "board_list"; // .HTML 연결
-}
+    }
 
     @GetMapping("/board_view/{id}") // 게시판 링크 지정
     public String board_view(Model model, @PathVariable Long id) {
@@ -79,5 +93,17 @@ public class BlogController {
         }
         return "board_view"; // .HTML 연결
     }
+
+    @GetMapping("/board_write")
+    public String board_write() {
+        return "board_write";
+    }
+
+    @PostMapping("/api/boards") // 글쓰기 게시판 저장
+    public String addboards(@ModelAttribute AddArticleRequest request) {
+        blogService.save(request);
+        return "redirect:/board_list"; // .HTML 연결
+    }
+//생
 }
 
--- demo/src/main/java/com/example/demo/model/service/BlogService.java
+++ demo/src/main/java/com/example/demo/model/service/BlogService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 //import com.example.demo.model.domain.Article;
 import com.example.demo.model.domain.Board;
 //import com.example.demo.model.repository.BlogRepository;
@@ -21,15 +24,15 @@ public class BlogService {
     // public List<Article> findAll() { // 게시판 전체 목록 조회
     //     return blogRepository.findAll();
     // }
-    // public Article save(AddArticleRequest request){
-    //     // DTO가 없는 경우 이곳에 직접 구현 가능
-    //     // public ResponseEntity<Article> addArticle(@RequestParam String title, @RequestParam String content) {
-    //     // Article article = Article.builder()
-    //     // .title(title)
-    //     // .content(content)
-    //     // .build();
-    //     return blogRepository.save(request.toEntity());
-    // }
+    public Board save(AddArticleRequest request){
+        // DTO가 없는 경우 이곳에 직접 구현 가능
+        // public ResponseEntity<Article> addArticle(@RequestParam String title, @RequestParam String content) {
+        // Article article = Article.builder()
+        // .title(title)
+        // .content(content)
+        // .build();
+        return blogRepository.save(request.toEntity());
+    }
     // public Optional<Article> findById(Long id) { // 게시판 특정 글 조회
     //     return blogRepository.findById(id);
     // }
@@ -41,6 +44,14 @@ public class BlogService {
     public Optional<Board> findById(Long id) { // 게시판 특정 글 조회
         return blogRepository.findById(id);
     }
+
+    public Page<Board> findAll(Pageable pageable) {
+        return blogRepository.findAll(pageable);
+    }
+        
+    public Page<Board> searchByKeyword(String keyword, Pageable pageable) {
+        return blogRepository.findByTitleContainingIgnoreCase(keyword, pageable);
+    } // LIKE 검색 제공(대소문자 무시)
 
     // public void update(Long id, AddArticleRequest request) {
     //     Optional<Article> optionalArticle = blogRepository.findById(id); // 단일 글 조회
