6326f35954c700c5745db84c6e1905d9d54e3225
admin <dbcksthd123@naver.com>
2024-11-27 11:41:04 +0900

    10주차

--- demo/src/main/java/com/example/demo/controller/BlogController.java
+++ demo/src/main/java/com/example/demo/controller/BlogController.java
@@ -19,6 +19,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.example.demo.model.domain.TestDB;
 import com.example.demo.model.repository.TestRepository;
 import com.example.demo.model.service.TestService;
+
+import jakarta.servlet.http.HttpSession;
+
 import com.example.demo.model.repository.BlogRepository;
 import com.example.demo.model.service.AddArticleRequest;
 import com.example.demo.model.service.BlogService;
@@ -65,7 +68,13 @@ public class BlogController {
     }
 
     @GetMapping("/board_list") // 새로운 게시판 링크 지정
-    public String board_list(Model model, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "") String keyword) {
+    public String board_list(Model model, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "") String keyword, HttpSession session) {
+        String userId = (String) session.getAttribute("userId"); // 세션 아이디 존재 확인
+        String email = (String) session.getAttribute("email"); // 세션에서 이메일 확인
+        if (userId == null) {
+            return "redirect:/member_login"; // 로그인 페이지로 리다이렉션
+        }
+        System.out.println("세션 userId: " + userId); // 서버 IDE 터미널에 세션 값 출력
         PageRequest pageable = PageRequest.of(page, 3); // 한 페이지의 게시글 수
         Page<Board> list; // Page를 반환
 
@@ -78,6 +87,7 @@ public class BlogController {
         model.addAttribute("totalPages", list.getTotalPages()); // 페이지 크기
         model.addAttribute("currentPage", page); // 페이지 번호
         model.addAttribute("keyword", keyword); // 키워드
+        model.addAttribute("email", email); // 로그인 사용자(이메일)
         return "board_list"; // .HTML 연결
     }
 
